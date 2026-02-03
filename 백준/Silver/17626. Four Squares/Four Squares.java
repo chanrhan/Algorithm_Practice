a880c4f25062a994acfe85d671db647290cf6305
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-03 14:39:07 +0900

    [Silver III] Title: Four Squares, Time: 252 ms, Memory: 128120 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+
+    public static int calc(int N) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(N);
+        int size, curr, num, pow, count = 0;
+        double min;
+        boolean[] visited = new boolean[50001];
+        while (!queue.isEmpty()) {
+            size = queue.size();
+            while (size > 0) {
+                curr = queue.poll();
+
+                visited[curr] = true;
+//                System.out.println("curr: " + curr);
+                if (curr == 0) {
+                    return count;
+                }
+                --size;
+                num = (int) Math.floor(Math.sqrt(curr));
+                min = Math.sqrt((double) curr / 2);
+                do {
+                    pow = (int) Math.pow(num, 2);
+                    if (curr == pow) {
+                        return count + 1;
+                    }
+                    if (curr - pow > 0) {
+                        if (!visited[curr - pow]) {
+                            queue.add(curr - pow);
+                        }
+                    }
+                    --num;
+                } while (num > 0);
+            }
+            ++count;
+        }
+        return count;
+    }
+
+    public static void main(String[] _s) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final int N = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString(calc(N)));
+
+        bw.close();
+        br.close();
+    }
+}
