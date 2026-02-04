1eca63339f4eb7faa1dafc7956b8ca14b7c09945
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-04 14:33:46 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 23028 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static List<List<Integer>> result = new ArrayList<>();
+    static int N, M;
+
+    public static class Node {
+        List<Integer> list;
+        int num;
+
+        public Node(int num, List<Integer> list) {
+            this.num = num;
+            this.list = list;
+        }
+
+        public Node keep() {
+            return new Node(num + 1, new ArrayList<>(list));
+        }
+
+        public Node add() {
+            List<Integer> newList = new ArrayList<>(list);
+            newList.add(num);
+            return new Node(num + 1, newList);
+        }
+
+        public Node addAndStay() {
+            List<Integer> newList = new ArrayList<>(list);
+            newList.add(num);
+            return new Node(num, newList);
+        }
+    }
+
+    public static String print(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); ++i) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] _s) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        Stack<Node> stack = new Stack<>();
+        stack.add(new Node(1, new ArrayList<>()));
+
+        Node curr;
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            if (curr.list.size() == M) {
+                result.add(curr.list);
+                continue;
+            }
+            if (curr.num > N || curr.list.size() > M) {
+                continue;
+            }
+            stack.push(curr.keep());
+            stack.push(curr.addAndStay());
+//            stack.push(curr.add());
+        }
+
+        for (int i = 0; i < result.size(); ++i) {
+            bw.write(print(result.get(i)) + '\n');
+        }
+
+        bw.close();
+        br.close();
+    }
+}
