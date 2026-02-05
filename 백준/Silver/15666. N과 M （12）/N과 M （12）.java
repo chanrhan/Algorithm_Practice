5a43c23a127e0d93aa75fa4a85ef090cd91bcbe6
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-05 16:41:37 +0900

    [Silver II] Title: N과 M (12), Time: 204 ms, Memory: 27212 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr;
+    static int[] ans;
+    static boolean[] visited;
+    static Set<String> set = new HashSet<>();
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            StringBuilder sb2 = new StringBuilder();
+            for (int i = 0; i < M; ++i) {
+                sb2.append(ans[i] + " ");
+            }
+            if (set.contains(sb2.toString())) {
+                return;
+            }
+            set.add(sb2.toString());
+            sb.append(sb2).append('\n');
+            return;
+        }
+        for (int i = 0; i < N; ++i) {
+            if (depth > 0 && ans[depth - 1] > arr[i]) {
+                continue;
+            }
+            ans[depth] = arr[i];
+            dfs(depth + 1);
+//            if (!visited[i]) {
+//                visited[i] = true;
+//                ans[depth] = arr[i];
+//                dfs(depth + 1);
+//                visited[i] = false;
+//            }
+        }
+    }
+
+    public static void main(String[] _s) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+        ans = new int[M];
+        tokenizer = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; ++i) {
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        bw.write(sb.toString());
+
+        bw.close();
+        br.close();
+    }
+}
