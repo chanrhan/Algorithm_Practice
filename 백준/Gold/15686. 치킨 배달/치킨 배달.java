16b7f2bf1dba55f9ee1bec63b1211a7275432e70
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-21 23:11:21 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 18044 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,125 +1,93 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
-
+ 
+class Point {
+    int x;
+    int y;
+ 
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+ 
 public class Main {
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static final int[][] directions = new int[][]{
-            {1, 0},
-            {0, 1},
-            {-1, 0},
-            {0, -1}
-    };
-
     static int N, M;
-    static Map<Point, Integer> houses = new HashMap<>();
-    static List<Point> chickens = new ArrayList<>();
-    static int min = Integer.MAX_VALUE;
-
-    public static class Point {
-        int r;
-        int c;
-
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Point p = (Point) obj;
-            return p != null && r == p.r && c == p.c;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("(%d,%d)", r, c);
-        }
-    }
-
-
-    public static void bt(int i, int count) {
-        if (count == M) {
-            min = Math.min(min, getSum());
-            return;
-        }
-        if (i >= chickens.size()) {
-            return;
-        }
-        bt(i + 1, count);
-
-        Point chicken = chickens.get(i);
-        Map<Point, Integer> rollback = new HashMap<>();
-        int d;
-        Integer org;
-        for (Point h : houses.keySet()) {
-            d = getDistance(chicken, h);
-            org = houses.get(h);
-            if (org == null || d < org) {
-                houses.put(h, d);
-                rollback.put(h, org);
-            }
-        }
-//        print(i, count + 1);
-        bt(i + 1, count + 1);
-        for (Point r : rollback.keySet()) {
-            houses.put(r, rollback.get(r));
-        }
-    }
-
-    public static int getDistance(Point a, Point b) {
-        return Math.abs(a.r - b.r) + Math.abs(a.c - b.c);
-    }
-
-    public static int getSum() {
-        int sum = 0;
-        for (Integer n : houses.values()) {
-            if (n != null) {
-                sum += n;
-            }
-        }
-        return sum;
-    }
-
-//    public static void print(int i, int count) {
-//        StringBuilder sb = new StringBuilder(String.format("--[ %d, %s : %d ]--\n", i, chickens.get(i), count));
-//        for (Point h : houses.keySet()) {
-//            sb.append(h).append(" : ").append(houses.get(h)).append('\n');
-//        }
-//        System.out.println(sb);
-//    }
-
-    public static void main(String[] _s) throws IOException {
-        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(tokenizer.nextToken());
-        M = Integer.parseInt(tokenizer.nextToken());
-
-        int n;
-        for (int r = 0; r < N; ++r) {
-            tokenizer = new StringTokenizer(br.readLine());
-            for (int c = 0; c < N; ++c) {
-                n = Integer.parseInt(tokenizer.nextToken());
-                if (n == 2) {
-                    chickens.add(new Point(r, c));
-                } else if (n == 1) {
-                    houses.put(new Point(r, c), null);
+    static int[][] map;
+    static ArrayList<Point> person;
+    static ArrayList<Point> chicken;
+    static int ans;
+    static boolean[] open;
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+ 
+        map = new int[N][N];
+        person = new ArrayList<>();
+        chicken = new ArrayList<>();
+ 
+        // 미리 집과 치킨집에 해당하는 좌표를 ArrayList에 넣어 둠.
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+ 
+                if (map[i][j] == 1) {
+                    person.add(new Point(i, j));
+                } else if (map[i][j] == 2) {
+                    chicken.add(new Point(i, j));
                 }
             }
         }
-
-        bt(0, 0);
-
-        bw.write(Integer.toString(min));
-
+ 
+        ans = Integer.MAX_VALUE;
+        open = new boolean[chicken.size()];
+ 
+        DFS(0, 0);
+        bw.write(ans + "\n");
+        bw.flush();
         bw.close();
         br.close();
     }
+ 
+    public static void DFS(int start, int cnt) {
+        if (cnt == M) {
+            int res = 0;
+ 
+            for (int i = 0; i < person.size(); i++) {
+                int temp = Integer.MAX_VALUE;
+ 
+                // 어떤 집과 치킨집 중 open한 치킨집의 모든 거리를 비교한다.
+                // 그 중, 최소 거리를 구한다.
+                for (int j = 0; j < chicken.size(); j++) {
+                    if (open[j]) {
+                        int distance = Math.abs(person.get(i).x - chicken.get(j).x)
+                                + Math.abs(person.get(i).y - chicken.get(j).y);
+ 
+                        temp = Math.min(temp, distance);
+                    }
+                }
+                res += temp;
+            }
+            ans = Math.min(ans, res);
+            return;
+        }
+ 
+        // 백트래킹
+        for (int i = start; i < chicken.size(); i++) {
+            open[i] = true;
+            DFS(i + 1, cnt + 1);
+            open[i] = false;
+        }
+    }
+ 
 }
