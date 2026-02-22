54f24efcb14558d4922347e114ee1c9c3c25708a
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-22 13:05:01 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 224 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,56 +13,44 @@ public class Main {
     static final int HORIZONTAL = 0;
     static final int VERTICAL = 1;
     static final int CROSS = 2;
+    static Integer[][][] dp;
+    static int count = 0;
 
-
-    public static class Pipe {
-        int r;
-        int c;
-        int shape;
-
-        public Pipe(int r, int c, int shape) {
-            this.r = r;
-            this.c = c;
-            this.shape = shape;
+    public static void dp(int r, int c, int shape) {
+        if (r == N - 1 && c == N - 1) {
+            ++count;
+            return;
+        }
+        if (dp[r][c][shape] == null) {
+            if (shape != HORIZONTAL && r + 1 < N && grid[r + 1][c] == 0) {
+                // 아래로 밀기
+                dp(r + 1, c, VERTICAL);
+            }
+            if (shape != VERTICAL && c + 1 < N && grid[r][c + 1] == 0) {
+                // 아래로 밀기
+                dp(r, c + 1, HORIZONTAL);
+            }
+            if (r + 1 < N && c + 1 < N && grid[r + 1][c] == 0 && grid[r][c + 1] == 0 && grid[r + 1][c + 1] == 0) {
+                // 아래로 밀기
+                dp(r + 1, c + 1, CROSS);
+            }
         }
     }
+
 
     public static void main(String[] _s) throws IOException {
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
         N = Integer.parseInt(tokenizer.nextToken());
         grid = new int[N][N];
+        dp = new Integer[N][N][3];
+
         for (int r = 0; r < N; ++r) {
             tokenizer = new StringTokenizer(br.readLine());
             for (int c = 0; c < N; ++c) {
                 grid[r][c] = Integer.parseInt(tokenizer.nextToken());
             }
         }
-
-        ArrayDeque<Pipe> deque = new ArrayDeque<>();
-        deque.add(new Pipe(0, 1, HORIZONTAL));
-
-        Pipe curr;
-        int count = 0;
-        while (!deque.isEmpty()) {
-            curr = deque.pop();
-            if (curr.r == N - 1 && curr.c == N - 1) {
-                ++count;
-                continue;
-            }
-            if (curr.shape != HORIZONTAL && curr.r + 1 < N && grid[curr.r + 1][curr.c] == 0) {
-                // 아래로 밀기
-                deque.add(new Pipe(curr.r + 1, curr.c, VERTICAL));
-            }
-            if (curr.shape != VERTICAL && curr.c + 1 < N && grid[curr.r][curr.c + 1] == 0) {
-                // 오른쪽으로 밀기
-                deque.add(new Pipe(curr.r, curr.c + 1, HORIZONTAL));
-            }
-            if (curr.r + 1 < N && curr.c + 1 < N && grid[curr.r + 1][curr.c] == 0 && grid[curr.r][curr.c + 1] == 0
-                    && grid[curr.r + 1][curr.c + 1] == 0) {
-                // 오른쪽 아래로 밀기
-                deque.add(new Pipe(curr.r + 1, curr.c + 1, CROSS));
-            }
-        }
+        dp(0, 1, HORIZONTAL);
         bw.write(Integer.toString(count));
 
         bw.close();
