4369332cc34b593c2a5a98f13358bcc658e998f8
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-02-27 21:35:50 +0900

    [Silver III] Title: N과 M (5), Time: 284 ms, Memory: 28560 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N, M;
     static int[] arr;
-    static int[] ans;
+    static int[] mem;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void dfs(int d) throws IOException {
-        if (d == M) {
+    public static void dp(int n, int count) {
+        if (count == M) {
             for (int i = 0; i < M; ++i) {
-                sb.append(ans[i] + " ");
+                sb.append(mem[i]).append(' ');
             }
             sb.append('\n');
             return;
@@ -28,44 +23,36 @@ public class Main {
         for (int i = 0; i < N; ++i) {
             if (!visited[i]) {
                 visited[i] = true;
-                ans[d] = arr[i];
-                dfs(d + 1);
+                mem[count] = arr[i];
+                dp(i, count + 1);
+                mem[count] = 0;
                 visited[i] = false;
             }
         }
     }
 
-
-    public static String print(List<Integer> list) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < list.size(); ++i) {
-            sb.append(list.get(i));
-            if (i < list.size() - 1) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
-
-    public static void main(String[] _s) throws IOException {
+    public static void main(String[] _s) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
         N = Integer.parseInt(tokenizer.nextToken());
         M = Integer.parseInt(tokenizer.nextToken());
-
         arr = new int[N];
-        ans = new int[M];
         visited = new boolean[N];
+        mem = new int[M];
+
         tokenizer = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; ++i) {
             arr[i] = Integer.parseInt(tokenizer.nextToken());
         }
         Arrays.sort(arr);
 
-        dfs(0);
-
+        dp(-1, 0);
         bw.write(sb.toString());
 
         bw.close();
         br.close();
     }
+
 }
+    
