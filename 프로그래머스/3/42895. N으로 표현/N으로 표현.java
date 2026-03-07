d8dc8371a3f45778e68e93403590f0cd5a9970d4
박희찬 <138795506+chanrhan@users.noreply.github.com>
2026-03-07 09:36:25 +0900

    [level 3] Title: N으로 표현, Time: 398.19 ms, Memory: 316 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        List<Set<Integer>> dp = new ArrayList<>();
-        for(int i=0;i<=8;++i){
-            dp.add(new HashSet<>());
-        }
+        List<List<Integer>> dp = new ArrayList<>();
+        dp.add(new ArrayList<>());
         
         for(int i=1;i<=8;++i){
-            Set<Integer> curr = dp.get(i);
+            List<Integer> curr = new ArrayList<>();
             
-            for(int k=1;k<i;++k){
-                for(int a : dp.get(k)){
-                    for(int b : dp.get(i - k)){
+            for(int k=1;k<i; ++k){
+                List<Integer> A = dp.get(k);
+                List<Integer> B = dp.get(i - k);
+                for(int a : A){
+                    for(int b : B){
                         curr.add(a + b);
-                        curr.add(a - b);
                         curr.add(a * b);
+                        curr.add(a - b);
                         if(b != 0){
-                            curr.add(a / b);                        
+                            curr.add(a / b);
                         }
                     }
                 }
             }
-            
             curr.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            
             if(curr.contains(number)){
                 return i;
             }
+            dp.add(curr);
         }
-        
-        
         
         return -1;
     }
